639d7fd8380f48546b4c90f26fd40d7e1fbb5dc4
LaurenLingham <lauren.lingham@outlook.com>
2023-04-28 15:14:42 +0100

    added Letter component

---
+++ src/GuessTheAnimal/Letter.tsx
@@ -0,0 +1,16 @@
+// import styles from Letter.css
+
+const Letter = ({letter}) => {
+
+    // const onClick = (e) => {
+    //   handleClick(e);
+    // }
+  
+    return (
+    <span className="letter">
+      {letter}
+    </span>)
+  }
+  
+  export default Letter;
+  
