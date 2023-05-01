bea7428d9ac3ef02954edc11b6fb2fb5a2c4fcb5
LaurenLingham <lauren.lingham@outlook.com>
2023-05-01 16:07:31 +0100

    added function to scramble letters

--- src/GuessTheAnimal/Components/AvailableLetters.tsx
+++ src/GuessTheAnimal/Components/AvailableLetters.tsx
@@ -17,8 +17,18 @@ const AvailableLetters = ({ wordToGuess, letterSelected }: { wordToGuess: string
             const additionalLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
             newLetters.push(<AvailableLetterTile letter={additionalLetter} letterSelected={letterSelected} />);
         }
+        
+        const scramble = (inputElements: JSX.Element[]): JSX.Element[] => {
+            const elements = [...inputElements];
+            for (let i = elements.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [elements[i], elements[j]] = [elements[j], elements[i]];
+            }
+            return elements
+        }
 
-        setAvailableLetters(newLetters);
+        const scrambledLetters = scramble(newLetters);
+        setAvailableLetters(scrambledLetters);
     };
 
     useEffect(() => {
@@ -28,6 +38,7 @@ const AvailableLetters = ({ wordToGuess, letterSelected }: { wordToGuess: string
     return (
         <>
             {...availableLetters}
+            {wordToGuess}
         </>
     )
 };
