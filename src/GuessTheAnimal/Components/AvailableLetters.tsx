9018a9910d6fa2b7c682857ade83457ca8f0f562
LaurenLingham <lauren.lingham@outlook.com>
2023-05-06 14:23:53 +0100

    fixed bug where available letters were being regenerated each time the current guess changed

--- src/GuessTheAnimal/Components/AvailableLetters.tsx
+++ src/GuessTheAnimal/Components/AvailableLetters.tsx
@@ -1,43 +1,58 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import AvailableLetterTile from "./AvailableLetterTile";
 
 const AvailableLetters = ({ wordToGuess, letterSelected }: { wordToGuess: string, letterSelected: (letter: string) => void }) => {
 
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const [displayedLetters, setDisplayedLetters] = useState("");
 
-    const letters: JSX.Element[] = [];
-    for (let character of wordToGuess) {
-        if (alphabet.includes(character)) {
-            alphabet = alphabet.replace(character, "");
+    useEffect(() => {
+        generateDisplayedLetters();
+    }, [wordToGuess]);
+
+    const generateDisplayedLetters = () => {
+        let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        let letters: string = "";
+        for (let character of wordToGuess) {
+            if (alphabet.includes(character)) {
+                alphabet = alphabet.replace(character, "");
+            }
+            letters += character;
         }
-        letters.push(<AvailableLetterTile letter={character} letterSelected={letterSelected} />);
+
+        const numberOfAdditionalLetters = 8 - wordToGuess.length;
+        const usedLetters = new Set(wordToGuess);
+        console.log(usedLetters);
+        for (let i = 0; i < numberOfAdditionalLetters; i++) {
+            let additionalLetter;
+            do {
+                additionalLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+            } while (usedLetters.has(additionalLetter));
+            usedLetters.add(additionalLetter);
+            letters += additionalLetter;
+        }
+
+        const scramble = (inputElements: string): string => {
+            const elements = inputElements.split("");
+            for (let i = elements.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [elements[i], elements[j]] = [elements[j], elements[i]];
+            }
+            return elements.join("");
+        };
+
+        const scrambledLetters = scramble(letters);
+
+        setDisplayedLetters(scrambledLetters);
     }
 
-    const numberOfAdditionalLetters = 8 - wordToGuess.length;
-    const usedLetters = new Set(letters.map((char) => char.props.letter));
-    for (let i = 0; i < numberOfAdditionalLetters; i++) {
-        let additionalLetter;
-        do {
-            additionalLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-        } while (usedLetters.has(additionalLetter));
-        usedLetters.add(additionalLetter);
-        letters.push(<AvailableLetterTile letter={additionalLetter} letterSelected={letterSelected} />);
-    }
-
-    const scramble = (inputElements: JSX.Element[]): JSX.Element[] => {
-        const elements = [...inputElements];
-        for (let i = elements.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [elements[i], elements[j]] = [elements[j], elements[i]];
-        }
-        return elements;
-    };
-
-    const scrambledLetters = scramble(letters);
+    const availableLetterTiles: JSX.Element[] = displayedLetters
+        .split("")
+        .map(character => <AvailableLetterTile letter={character} letterSelected={letterSelected} />)
 
     return (
         <div className="letters-container">
-            {...scrambledLetters}
+            {...availableLetterTiles}
         </div>
     )
 };
