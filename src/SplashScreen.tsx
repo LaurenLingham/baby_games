aa988d174f9f011affbeee998da36c98e81565f3
LaurenLingham <lauren.lingham@outlook.com>
2023-05-09 12:55:26 +0100

    styling

--- src/SplashScreen.tsx
+++ src/SplashScreen.tsx
@@ -2,6 +2,8 @@ import "./SplashScreen.css";
 import Lottie from "lottie-react";
 import purpleStars from "./purpleStars.json";
 import ParticlesBackground from "./ParticlesBackground";
+import animalGame from "./Images/animal_game.jpeg";
+import carrotGame from "./Images/carrot_game.jpeg";
 
 const SplashScreen = (
     { handleGuessTheAnimalGameSelected, handleCollectTheCarrotsGameSelected }:
@@ -17,14 +19,16 @@ const SplashScreen = (
 
     return (
         <div className="splash-screen">
-            <ParticlesBackground />
+            {/* <ParticlesBackground /> */}
             <Lottie className="animation" animationData={purpleStars} loop={true} />
             <div className="splash-buttons">
                 <div className="game-link animal-game">
+                    <img src={ animalGame } alt="" className="game-image" />
                     <p>Guess The Animal</p>
                     <button onClick={ onAnimalButtonClicked }>Play</button>
                 </div>
                 <div className="game-link carrot-game">
+                    <img src={ carrotGame } alt="" className="game-image" />
                     <p>Collect The Carrots</p>
                     <button onClick={ onCarrotButtonClicked }>Play</button>
                 </div>
