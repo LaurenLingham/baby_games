12c4d049633f454658015e396c8eb96b697df0d2
LaurenLingham <lauren.lingham@outlook.com>
2023-05-09 14:43:17 +0100

    minor styling change

--- src/CollectTheCarrots/Components/ProgressPanel.tsx
+++ src/CollectTheCarrots/Components/ProgressPanel.tsx
@@ -5,17 +5,17 @@ const ProgressPanel = ({ collectedCarrots, handleSplashScreenReturn }: { collect
 
     return (
         <div className="progress-panel">
-            <button className="home-button" onClick={handleSplashScreenReturn}>Back to Games</button>
-            {collectedCarrots < 10
+            <button className="home-button" onClick={ handleSplashScreenReturn }>Back to Games</button>
+            { collectedCarrots < 10
                 ? <div>
-                    <img src={bunnyWithBasket} alt="" width="350px" />
-                    <p>{collectedCarrots} carrots collected</p>
-                    <p>{10 - collectedCarrots} left to find</p>
+                    <img src={ bunnyWithBasket } className="bunny-image" alt="" width="350px" />
+                    <p>{ collectedCarrots } carrots collected</p>
+                    <p>{ 10 - collectedCarrots } left to find</p>
                 </div>
                 : <div>
-                    <img src={bunnyWithCarrot} alt="" width="350px" />
+                    <img src={ bunnyWithCarrot } className="win-image" alt="" width="350px" />
                     <p>Congratulations! <br /> You found all of the carrots</p>
-                    <button className="play-again-button" onClick={handleSplashScreenReturn}>Play again</button>
+                    <button className="play-again-button" onClick={ handleSplashScreenReturn }>Play again</button>
                 </div>
             }
         </div>
